b3caf053cd3894b4be3071960b481b8cabf414f0
amsalpakpahan03 <pakpahanamsal@gmail.com>
2025-12-07 05:42:32 +0700

    ubah

--- server/index.js
+++ server/index.js
@@ -8,13 +8,19 @@ const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-  
+
 // Connect MongoDB
 connectDB();
 
 // CORS fix (allow all for deploy)
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 // folder uploads
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
