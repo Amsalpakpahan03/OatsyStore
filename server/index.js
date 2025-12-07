735c6b8d30da79a296a9292f0d17ef931afa57b6
amsalpakpahan03 <pakpahanamsal@gmail.com>
2025-12-07 07:25:22 +0700

    lagi

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const reviewRoutes = require("./routes/reviewRoutes");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Connect MongoDB
 connectDB();
